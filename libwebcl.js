7b27ac7576943759fc19cde8854cbe7a0f93a928
Alexandre Rocha <alerock@gmail.com>
2013-06-06 16:43:10 -0400

    Removing unnecessary logs
    Reviewed by: NOBODY

--- libwebcl.js
+++ libwebcl.js
@@ -120,8 +120,6 @@
         API['CL_PLATFORM_EXTENSIONS'] = 0x904;
         API['CL_DEVICE_EXTENSIONS'] = 0x1030;
 
-        console.log("WebCL :", nativeWebCL);
-
         return API;
     })();
 
@@ -569,8 +567,6 @@
 
                 var gws = new Int32Array(globalWorkSize);
                 var lws = new Int32Array(localWorkSize);
-
-                console.log("gws", [gws], "lws", [lws]);
 
                 nativeCmdQueue.enqueueNDRangeKernel(hostKernel.getNative(),
                         null, gws, lws);
